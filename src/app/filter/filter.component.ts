8cb5b9677698146634b6a218a94e0139b208fd98
Naqeeb4404 <naqeeblat@gmail.com>
2023-01-23 16:39:30 +0800

    ads

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit{
+  constructor(){ }
+
+  ngOnInit(): void {
+    
+  }
+  all: number = 0;
+  free: number = 0;
+  premium: number = 0;
 
 }
